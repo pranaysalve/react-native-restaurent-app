be21b4905882a366d8481bb4ee78d71e592afdad
Pranay Salve <64769068+pranaysalve@users.noreply.github.com>
2023-07-31 01:59:31 +0530

    Update App.js

--- App.js
+++ App.js
@@ -20,27 +20,12 @@ import { Navigation } from "./src/infrastructure/navigation";
 
 import { AuthenticationContextProvider } from "./src/services/authentication/authentication_context";
 
-// import * as firebase from "firebase";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBalpkjTI_NnC3gfNrt2_P_zw6Z6XH8KlE",
-//   authDomain: "mealstogo-64712.firebaseapp.com",
-//   projectId: "mealstogo-64712",
-//   storageBucket: "mealstogo-64712.appspot.com",
-//   messagingSenderId: "271007920837",
-//   appId: "1:271007920837:web:3981383a4ea9f4e5fcb6ac",
-// };
 
-// if (!firebase.apps.length) {
-//   firebase.initializeApp(firebaseConfig);
-// } else {
-//   firebase.app(); // if already initialized, use that one
-// }
 
 import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBalpkjTI_NnC3gfNrt2_P_zw6Z6XH8KlE",
+  apiKey: "API KEY",
   authDomain: "mealstogo-64712.firebaseapp.com",
   projectId: "mealstogo-64712",
   storageBucket: "mealstogo-64712.appspot.com",
